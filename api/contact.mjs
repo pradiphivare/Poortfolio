665db49d83227ccf4b0e4123c0cc237bd7f5418a
pradip_hivare <pradiphivare2510@gmail.com>
2025-01-05 14:29:24 +0530

    uop

--- api/contact.mjs
+++ api/contact.mjs
@@ -14,10 +14,8 @@ export default async (req, res) => {
 
     if (!mongoose.connection.readyState) {
       try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Successfully connected to MongoDB!");
       } catch (error) {
         console.error('Error connecting to MongoDB:', error);
         return res.status(500).send('Failed to connect to database');
@@ -28,6 +26,7 @@ export default async (req, res) => {
 
     try {
       await newContact.save();
+      console.log("Message saved to database");
       res.status(200).send('Message received and saved to database');
     } catch (error) {
       console.error('Error saving message to database:', error);
