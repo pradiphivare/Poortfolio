ef91e0d2e403d3776e2de3eff89f46a7c2278276
Pradip Hivare <102855007+pradiphivare@users.noreply.github.com>
2025-04-29 10:16:06 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -12,6 +12,58 @@ export default function About() {
           About Me
         </h2>
         <div className="grid md:grid-cols-2 gap-12 items-center">
+          {/* Text Section */}
+          <div>
+            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
+              I’m <span className="font-bold text-black">Pradip Hivare</span>, a DevOps Engineer with a strong focus on cloud infrastructure, automation, and observability. Currently, I work at <span className="font-semibold text-black">Tata Elxsi</span>, where I design and optimize CI/CD pipelines using <span className="font-bold text-black">Jenkins</span> and <span className="font-bold text-black">GitHub Actions</span>, automate infrastructure provisioning with <span className="font-bold text-black">Terraform</span>, <span className="font-bold text-black">Ansible</span>, and <span className="font-bold text-black">Helm</span>, and manage containerized environments with <span className="font-bold text-black">Docker</span> and <span className="font-bold text-black">Kubernetes</span>.
+            </p>
+            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
+              With expertise in observability tools like <span className="font-bold text-black">Victoria Metrics</span>, <span className="font-bold text-black">Grafana Cloud</span>, <span className="font-bold text-black">Prometheus</span>, and <span className="font-bold text-black">Datadog</span>, I ensure seamless infrastructure performance and security across hybrid cloud systems. I have a proven track record of improving operational efficiency, reducing support tickets by 30-50%, and enhancing incident response times by automating workflows, such as building a Slack bot for automated Jira ticket creation.
+            </p>
+            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
+              I have a deep understanding of <span className="font-bold text-black">AWS services</span>, including Lambda, EC2, S3, and EBS, and have automated infrastructure provisioning while optimizing application performance. My experience also includes leveraging <span className="font-bold text-black">Infrastructure as Code (IaC)</span> and <span className="font-bold text-black">GitOps</span> methodologies to streamline workflows and collaborate effectively across teams, enhancing development processes.
+            </p>
+            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
+              Passionate about continuous learning, I stay ahead of the curve with the latest in cloud-native technologies, DevOps practices, and open-source contributions, driving innovation in every project I take on.
+            </p>
+
+            {/* Expertise Points */}
+            <div className="space-y-6">
+              <div className="flex items-center space-x-6">
+                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-300">
+                  <Code2 className="w-12 h-12 text-teal-600" />
+                </div>
+                <span className="text-lg font-medium text-black">
+                  Expertise in Automation and CI/CD
+                </span>
+              </div>
+              <div className="flex items-center space-x-6">
+                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-300">
+                  <Server className="w-12 h-12 text-blue-600" />
+                </div>
+                <span className="text-lg font-medium text-black">
+                  Advanced Observability Practices
+                </span>
+              </div>
+              <div className="flex items-center space-x-6">
+                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-300">
+                  <Cloud className="w-12 h-12 text-indigo-600" />
+                </div>
+                <span className="text-lg font-medium text-black">
+                  Cloud Expertise: AWS, Terraform, Kubernetes
+                </span>
+              </div>
+              <div className="flex items-center space-x-6">
+                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-105 transition-transform duration-300">
+                  <Shield className="w-12 h-12 text-yellow-600" />
+                </div>
+                <span className="text-lg font-medium text-black">
+                  Security and Monitoring Integrations
+                </span>
+              </div>
+            </div>
+          </div>
+
           {/* Image Section */}
           <div className="relative">
             <img
@@ -21,58 +73,6 @@ export default function About() {
             />
             <div className="absolute -top-8 -left-8 w-24 h-24 bg-teal-200 rounded-full animate-pulse"></div>
             <div className="absolute -bottom-8 -right-8 w-40 h-40 bg-blue-100 rounded-full"></div>
-          </div>
-
-          {/* Text Section */}
-          <div>
-            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
-              I’m <span className="font-bold text-blue-600">Pradip Hivare</span>, a DevOps Engineer with a strong focus on cloud infrastructure, automation, and observability. Currently, I work at <span className="font-semibold text-gray-900">Tata Elxsi</span>, where I design and optimize CI/CD pipelines using <span className="font-bold text-blue-600">Jenkins</span> and <span className="font-bold text-blue-600">GitHub Actions</span>, automate infrastructure provisioning with <span className="font-bold text-blue-600">Terraform</span>, <span className="font-bold text-blue-600">Ansible</span>, and <span className="font-bold text-blue-600">Helm</span>, and manage containerized environments with <span className="font-bold text-blue-600">Docker</span> and <span className="font-bold text-blue-600">Kubernetes</span>.
-            </p>
-            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
-              With expertise in observability tools like <span className="font-bold text-blue-600">Victoria Metrics</span>, <span className="font-bold text-blue-600">Grafana Cloud</span>, <span className="font-bold text-blue-600">Prometheus</span>, and <span className="font-bold text-blue-600">Datadog</span>, I ensure seamless infrastructure performance and security across hybrid cloud systems. I have a proven track record of improving operational efficiency, reducing support tickets by 30-50%, and enhancing incident response times by automating workflows, such as building a Slack bot for automated Jira ticket creation.
-            </p>
-            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
-              I have a deep understanding of <span className="font-bold text-blue-600">AWS services</span>, including Lambda, EC2, S3, and EBS, and have automated infrastructure provisioning while optimizing application performance. My experience also includes leveraging <span className="font-bold text-blue-600">Infrastructure as Code (IaC)</span> and <span className="font-bold text-blue-600">GitOps</span> methodologies to streamline workflows and collaborate effectively across teams, enhancing development processes.
-            </p>
-            <p className="text-lg text-gray-800 mb-6 leading-relaxed">
-              Passionate about continuous learning, I stay ahead of the curve with the latest in cloud-native technologies, DevOps practices, and open-source contributions, driving innovation in every project I take on.
-            </p>
-
-            {/* Expertise Cards with Color Highlights */}
-            <div className="space-y-6">
-              <div className="flex items-center space-x-6">
-                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-110 transition-transform duration-300">
-                  <Code2 className="w-12 h-12 text-teal-600" />
-                </div>
-                <span className="text-lg font-medium text-gray-900">
-                  <span className="font-bold text-black">Expertise in</span> Automation and CI/CD
-                </span>
-              </div>
-              <div className="flex items-center space-x-6">
-                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-110 transition-transform duration-300">
-                  <Server className="w-12 h-12 text-indigo-600" />
-                </div>
-                <span className="text-lg font-medium text-gray-900">
-                  <span className="font-bold text-black">Advanced</span> Observability Practices
-                </span>
-              </div>
-              <div className="flex items-center space-x-6">
-                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-110 transition-transform duration-300">
-                  <Cloud className="w-12 h-12 text-blue-600" />
-                </div>
-                <span className="text-lg font-medium text-gray-900">
-                  <span className="font-bold text-black">Cloud Expertise:</span> AWS, Terraform, Kubernetes
-                </span>
-              </div>
-              <div className="flex items-center space-x-6">
-                <div className="p-6 bg-white rounded-xl shadow-lg transform hover:scale-110 transition-transform duration-300">
-                  <Shield className="w-12 h-12 text-yellow-600" />
-                </div>
-                <span className="text-lg font-medium text-gray-900">
-                  <span className="font-bold text-black">Security and</span> Monitoring Integrations
-                </span>
-              </div>
-            </div>
           </div>
         </div>
       </div>
